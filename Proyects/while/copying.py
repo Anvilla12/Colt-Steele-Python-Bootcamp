76eed0b9c969a328b49f7c0112f9603db012bb21
Antonio Cortes <anavila.developer@gmail.com>
2022-02-23 23:50:09 -0600

    add copying exercise

---
+++ Proyects/while/copying.py
@@ -0,0 +1,9 @@
+print('''==========================================================
+                < Copying Game >
+==========================================================''')
+phrase = input('how you doing? ')
+
+while phrase != 'Stop':
+    phrase = input(f'{phrase} ')
+
+2print('\U0001F612 Ok, fine... ')
