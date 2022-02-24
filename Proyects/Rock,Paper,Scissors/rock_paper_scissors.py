f32fe75c2bce9eea6b8d2e82f75a7eb9a4b58941
Antonio Cortes <anavila.developer@gmail.com>
2022-02-24 13:49:09 -0600

    add things to rock papr n sciss game

--- Proyects/Rock,Paper,Scissors/rock_paper_scissors.py
+++ Proyects/Rock,Paper,Scissors/rock_paper_scissors.py
@@ -1,25 +1,48 @@
 from random import choice
-npc = choice(['rock', 'paper', 'scissors'])
+print('''=====================================================================
+                Rock ✊, Paper ✋ n Scissors ✌
+=====================================================================
+''')
 
-player_1 = input('Select a choice \n 1. rock \n 2. paper \n 3. scissors \n').lower()
+player_wins = 0
+npc_wins = 0
 
-if player_1 == npc:
-    print(f'PC chooses {npc} \n Its a Draw!!! ')
-elif player_1 == 'rock' and npc == 'paper':
-    print(f'PC chooses {npc} \n You Lose!!! ')
-elif player_1 == 'rock' and npc == 'scissors':
-    print(f'PC chooses {npc} \n You Win!!! ')
+# score = print(f'''
+# P1: {player_wins}
+# PC: {npc_wins}
+# ''')
 
-elif player_1 == 'paper' and npc == 'rock':
-    print(f'PC chooses {npc} \n You Win!!! ')
-elif player_1 == 'paper' and npc == 'scissors':
-    print(f'PC chooses {npc} \n You Lose!!! ')
+while player_wins < 3 and npc_wins < 3:
 
-elif player_1 == 'scissors' and npc == 'rock':
-    print(f'PC chooses {npc} \n You Lose!!! ')
-elif player_1 == 'scissors' and npc == 'paper':
-    print(f'PC chooses {npc} \n You Win!!! ')
+    npc = choice(['rock', 'paper', 'scissors'])
 
+    player_1 = input(
+        'Select a choice \n 1. rock \n 2. paper \n 3. scissors \n').lower()
 
-else:
-    print('Something go wrong')
+    if player_1 == npc:
+        print(f'PC chooses {npc} \n Its a Draw!!! ')
+    elif player_1 == 'rock' and npc == 'paper':
+        print(f'PC chooses {npc} \n You Lose!!! ')
+        npc_wins += 1
+    elif player_1 == 'rock' and npc == 'scissors':
+        print(f'PC chooses {npc} \n You Win!!! ')
+        player_wins += 1
+    elif player_1 == 'paper' and npc == 'rock':
+        print(f'PC chooses {npc} \n You Win!!! ')
+        player_wins += 1
+    elif player_1 == 'paper' and npc == 'scissors':
+        print(f'PC chooses {npc} \n You Lose!!! ')
+        npc_wins += 1
+    elif player_1 == 'scissors' and npc == 'rock':
+        print(f'PC chooses {npc} \n You Lose!!! ')
+        npc_wins += 1
+    elif player_1 == 'scissors' and npc == 'paper':
+        print(f'PC chooses {npc} \n You Win!!! ')
+        player_wins += 1
+    else:
+        print('Something go wrong')
+
+    print(f'''
+                                P1: {player_wins}
+                                PC: {npc_wins}
+''')
