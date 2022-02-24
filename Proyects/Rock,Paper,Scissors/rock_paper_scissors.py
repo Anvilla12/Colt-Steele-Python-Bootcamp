14666ff3435500e2519c5aaca39a4a4a95626563
Antonio Cortes <anavila.developer@gmail.com>
2022-02-23 19:18:49 -0600

    Rock, paper n scissors by my own

---
+++ Proyects/Rock,Paper,Scissors/rock_paper_scissors.py
@@ -0,0 +1,28 @@
+from random import choice
+npc = choice(['rock', 'paper', 'scissors'])
+
+player_1 = input('Select a choice \n 1. rock \n 2. paper \n 3. scissors \n'.lower())
+
+if player_1 == 'rock' and npc == 'rock':
+    print(f'PC chooses {npc} \n Its a Draw!!! ')
+elif player_1 == 'rock' and npc == 'paper':
+    print(f'PC chooses {npc} \n You Lose!!! ')
+elif player_1 == 'rock' and npc == 'scissors':
+    print(f'PC chooses {npc} \n You Win!!! ')
+
+elif player_1 == 'paper' and npc == 'rock':
+    print(f'PC chooses {npc} \n You Win!!! ')
+elif player_1 == 'paper' and npc == 'paper':
+    print(f'PC chooses {npc} \n Its a Draw!!! ')
+elif player_1 == 'paper' and npc == 'scissors':
+    print(f'PC chooses {npc} \n You Lose!!! ')
+
+elif player_1 == 'scissors' and npc == 'rock':
+    print(f'PC chooses {npc} \n You Lose!!! ')
+elif player_1 == 'scissors' and npc == 'paper':
+    print(f'PC chooses {npc} \n You Win!!! ')
+elif player_1 == 'scissors' and npc == 'scissors':
+    print(f'PC chooses {npc} \n Its a Draw!!! ')
+
+else:
+    print('Something go wrong')
